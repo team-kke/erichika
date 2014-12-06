7e33179406372482e0cfba1f14b5959883ae7fa3
Jeong Yong-uk <ywj1022@gmail.com>
2014-12-06 19:48:16 +0900

    Integrate socket.io to the backend.
    with dummy lines.

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 
 var debug = require('debug')('server');
 var express = require('express');
+var io = require('socket.io')();
 var path = require('path');
 
 var routes = require('./routes/index');
@@ -18,4 +19,17 @@ var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port;
 
   debug('listening at http://%s:%s', host, port);
+});
+
+io.attach(server);
+
+io.on('connection', function (socket) {
+  debug('+1 socket connection');
+  io.emit('test', { message: 'Hey, everyone! +1 connection' });
+  socket.on('test', function (data) {
+    debug('received: %s', data);
+  });
+  socket.on('disconnect', function () {
+    debug('-1 socket connection');
+  });
 });
