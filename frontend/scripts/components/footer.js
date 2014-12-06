062b053240064e80032a7898bfcbcfe7b58d7654
Hyunje Alex Jun <me@noraesae.net>
2014-12-06 18:07:19 +0000

    Change footer link.
    Not to cover whole footer div element.

--- frontend/scripts/components/footer.js
+++ frontend/scripts/components/footer.js
@@ -6,11 +6,13 @@ var FooterComponent = React.createClass({
   render: function () {
     return (
       <div id='footer'>
-        <a className='wrapper' href='https://github.com/team-kke'>
-          <div className='left'>2014</div>
-          <img src='https://avatars2.githubusercontent.com/u/10048536?v=3&s=200' />
-          <div className='right'>KKE</div>
-        </a>
+        <div className='wrapper'>
+          <a href='https://github.com/team-kke' className='text left'>2014</a>
+          <a href='https://github.com/team-kke' className='profile'>
+            <img src='https://avatars2.githubusercontent.com/u/10048536?v=3&s=200' />
+          </a>
+          <a href='https://github.com/team-kke' className='text right'>KKE</a>
+        </div>
       </div>
     );
   }
