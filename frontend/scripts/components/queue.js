8f1d16a1231db8313fedbc142c1d1a32006e6e0a
Hyunje Alex Jun <me@noraesae.net>
2014-12-07 13:25:44 +0000

    Add queue/confirm UI.

--- frontend/scripts/components/queue.js
+++ frontend/scripts/components/queue.js
@@ -5,7 +5,8 @@ var React = require('react/addons');
 var // Queue State Enums
   Normal = 0,
   Queueing = 1,
-  Confirmed = 2;
+  WaitingConfirm = 2,
+  Confirmed = 3;
 
 var // Constants
   MaxPlayerCount = 6;
@@ -32,11 +33,46 @@ var QueueComponent = React.createClass({
           <div className='button-text-wrapper'>
             <span className='button-text queueing'>
               Queueing...
-              <span ref='queueCount' className='button-text count'></span>
+              <span ref='queueCount' className='marg-l3'></span>
             </span>
             <span className='button-text cancel'>Cancel</span>
           </div>
         </a>
+      );
+    case WaitingConfirm:
+      return (
+        <div>
+          <a className='button left text' href='#'>
+            <span>Be ready...</span>
+            <span ref='queueCount' className='marg-l3'></span>
+          </a>
+          <a className='button left confirm' href='#' onClick={this.confirm}>
+            <div className='check'>✓</div>
+            <div className='button-text-wrapper'>
+              <span className='button-text'>
+                I&rsquo;m ready!
+              </span>
+            </div>
+          </a>
+          <a className='button left dodge' href='#' onClick={this.dodge}>
+            No
+          </a>
+        </div>
+      );
+    case Confirmed:
+      return (
+        <div>
+          <a className='button left confirmed' href='#'>
+            <div className='check'>✓</div>
+            <div className='button-text-wrapper'>
+              <span>I&rsquo;m ready!</span>
+              <span ref='queueCount' className='marg-l3'></span>
+            </div>
+          </a>
+          <a className='button left dodge' href='#' onClick={this.dodge}>
+            No
+          </a>
+        </div>
       );
     }
     return null;
@@ -54,14 +90,28 @@ var QueueComponent = React.createClass({
   update: function (data) {
     switch (data.state) {
     case 'wait-player':
-      if (this.refs.queueCount) {
-        var count = this.refs.queueCount.getDOMNode();
-        count.innerText = data.current + ' / ' + MaxPlayerCount;
-      }
-      return;
+      break;
     case 'wait-confirm':
-      // TODO
+      if (this.state.queueState !== WaitingConfirm) {
+        this.setState({queueState: WaitingConfirm});
+      }
+      break;
     }
+
+    if (this.refs.queueCount) {
+      var count = this.refs.queueCount.getDOMNode();
+      count.innerText = data.current + ' / ' + MaxPlayerCount;
+    }
+  },
+  confirm: function (e) {
+    this.setState({queueState: Confirmed});
+    this.props.socket.emit('queue/confirm');
+    e.preventDefault();
+  },
+  dodge: function (e) {
+    this.setState({queueState: Queueing});
+    this.props.socket.emit('queue/dodge');
+    e.preventDefault();
   }
 });
 
